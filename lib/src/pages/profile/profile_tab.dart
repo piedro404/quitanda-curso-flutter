a16cd6306ce4b43426274c09c62594e8b5db6cc8
Pedro Henrique <pedro.henrique.martins404@gmail.com>
2024-09-04 10:46:40 -0300

    feat: :seedling: Add Dialog for update password

--- lib/src/pages/profile/profile_tab.dart
+++ lib/src/pages/profile/profile_tab.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:quitanda/src/pages/common_widgets/custom_text_field.dart';
 import 'package:quitanda/src/config/app_data.dart' as appData;
 
-class ProfileTab extends StatelessWidget {
+class ProfileTab extends StatefulWidget {
   const ProfileTab({super.key});
 
+  @override
+  State<ProfileTab> createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,7 +56,9 @@ class ProfileTab extends StatelessWidget {
           SizedBox(
             height: 50,
             child: OutlinedButton(
-              onPressed: () {},
+              onPressed: () {
+                updatePassword();
+              },
               child: const Text(
                 'Atualizar Senha',
               ),
@@ -59,6 +66,83 @@ class ProfileTab extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  Future<bool?> updatePassword() {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 12,
+                      ),
+                      child: Text(
+                        'Atualização de senha',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                    const CustomTextField(
+                      icon: Icons.lock,
+                      label: "Senha atual",
+                      isSecret: true,
+                    ),
+                    const CustomTextField(
+                      icon: Icons.lock_outline,
+                      label: "Nova senha",
+                      isSecret: true,
+                    ),
+                    const CustomTextField(
+                      icon: Icons.lock_outline,
+                      label: "Confirmar nova senha",
+                      isSecret: true,
+                    ),
+                    SizedBox(
+                      height: 45,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: const Text(
+                          'Atualizar',
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                top: 17,
+                right: 5,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  icon: const Icon(Icons.close),
+                ),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
